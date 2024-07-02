9f6a3315045a4dd369d3ebbd9388d272d463464a
Bill Vasfaret <125101202+SkullKid2113@users.noreply.github.com>
2024-07-01 19:47:18 -0500

    all tests passing

--- exercises/e3.js
+++ exercises/e3.js
@@ -1,6 +1,8 @@
 // FUNCTIONS: Early return
 // BEFORE YOU START: please read the /exercises-info/e3.md file.
 
+import { log } from 'console';
+
 /**
  * ====================================================
  * Create a function that works as follows:
@@ -14,10 +16,15 @@
  * doesArrayIncludeItemsBetweenVals([2, 4, 2], 5, 10) => false
  */
 
-// Your code goes here...
-
-
-
+function doesArrayIncludeItemsBetweenVals(arr, val1, val2) {
+  for (let arrItem of arr) {
+    console.log(arrItem);
+    if (arrItem > val1 && arrItem < val2) {
+      return true;
+    }
+  }
+  return false;
+}
 
 /**
  * ====================================================
@@ -27,29 +34,48 @@
  * Arrow Function name: getValueWithConditionTwo;
  * Variable name for the function expression: getValueWithConditionThree;
  * All three functions resolve the same task:
-    * The function takes two numbers as arguments
-    * If the numbers are both equal to 40, return the sum of them
-    * Else return the doubled sum of them
+ * The function takes two numbers as arguments
+ * If the numbers are both equal to 40, return the sum of them
+ * Else return the doubled sum of them
  * getValueWithConditionOne(40, 40) => 80;
  * getValueWithConditionOne(20, 30) => 100;
  */
 
-// Your code goes here...
+function getValueWithConditionOne(num1, num2) {
+  if (num1 === 40 && num2 === 40) {
+    return Number(num1 + num2);
+  } else {
+    return Number((num1 + num2) * 2);
+  }
+}
 
+const getValueWithConditionTwo = (num1, num2) => {
+  if (num1 === 40 && num2 === 40) {
+    return Number(num1 + num2);
+  } else {
+    return Number((num1 + num2) * 2);
+  }
+};
 
-
+const getValueWithConditionThree = function (num1, num2) {
+  if (num1 === 40 && num2 === 40) {
+    return Number(num1 + num2);
+  } else {
+    return Number((num1 + num2) * 2);
+  }
+};
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-3"
 // If the test has all tests passed, switch to the next exercise file
 // If any of the tests fails, refactor the code and run the test command after you've fixed the function
 
-
 // Do not delete or change the lines beneath
 const f = {
-  doesArrayIncludeItemsBetweenVals: doesArrayIncludeItemsBetweenVals || undefined,
+  doesArrayIncludeItemsBetweenVals:
+    doesArrayIncludeItemsBetweenVals || undefined,
   getValueWithConditionOne: getValueWithConditionOne || undefined,
   getValueWithConditionTwo: getValueWithConditionTwo || undefined,
   getValueWithConditionThree: getValueWithConditionThree || undefined,
-}
+};
 export { f };
